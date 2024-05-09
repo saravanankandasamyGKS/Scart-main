a1b8b5cda38dc22ab49d01e1a293c86fd3c0b6dc
saravanankandasamyGKS <gksaravanan008@gmail.com>
2024-05-09 16:57:58 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const cors=require("cors")
 const app = express();
 const errorMiddleware = require('./middlewares/error');
 const cookieParser = require('cookie-parser')
 const path = require('path')
 const dotenv = require('dotenv');
 dotenv.config({path:path.join(__dirname,"config/config.env")});
-
-app.use(cors({
-
-    origin:"http://localhost:3000"
-}))
-
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
